6fe0d2f37864e74031fff368938620ab668fa3c6
Murad268 <82538242+Murad268@users.noreply.github.com>
2022-08-26 21:57:50 +0400

    commit

--- src/components/FavAndWatch/FavAndWatch.jsx
+++ src/components/FavAndWatch/FavAndWatch.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {Row, Typography, Col} from 'antd';
 import styles from './fandw.module.scss'
 import MainButton from '../MainMovie/MainMovie';
+import empty from '../../assets/images/ae8ac2fa217d23aadcc913989fcc34a2---page-empty-page.jpg'
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import './fandw.module.scss'
@@ -22,9 +23,9 @@ const FavAndWatch = () => {
                <Col span={16} offset={2}><Title className={styles.title}>my {pathname.slice(1).replace("_", " ")} movies</Title></Col>
                <Row style={{"padding": "38px 0 0 90px"}} gutter={[16, 16]}>
                   {
-                     moviesList?.map(movie => {
+                     !!moviesList.length?moviesList.map(movie => {
                         return <MainButton key={movie.id} movie={movie}/>
-                     })
+                     }):<div className={styles.empty}><img src={empty} alt="" /></div>
                   }
                </Row>
               
