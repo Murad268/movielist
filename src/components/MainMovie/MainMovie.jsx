aefe2a22a15678662564f8dd8de695f76bb7e735
Murad268 <82538242+Murad268@users.noreply.github.com>
2022-08-26 14:01:29 +0400

    commit

--- src/components/MainMovie/MainMovie.jsx
+++ src/components/MainMovie/MainMovie.jsx
@@ -5,7 +5,11 @@ import star from '../../assets/icons/Vector.png';
 import notFound from '../../assets/images/notfound.png';
 const MainMovie = ({movie}) => {
    return (
-      <Col span={5} className={styles.movie} >
+      <Col span={5} className={styles.movie}>
+         <div className={styles.movie__controlls}>
+            <div className="favorite">💙</div>
+            <div className="watch">👁</div>
+         </div>
          <div className={styles.movie__raiting}><img src={star} alt="" /><div>{movie.vote_average}</div></div>
          {
             <img src={movie.poster_path?`https://image.tmdb.org/t/p/w500/${movie.poster_path}`:notFound} alt="" />
