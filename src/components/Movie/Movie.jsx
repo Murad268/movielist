4eff6afa1e4a7f47bd1b4b618870ecf9f403f129
Murad268 <82538242+Murad268@users.noreply.github.com>
2022-08-27 10:45:41 +0400

    commit

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -21,17 +21,17 @@ const Movie = () => {
    return (
       <>
         <Layout className={styles.movie}>
-         <Image height={480} src={"https://image.tmdb.org/t/p/w500/"+details.backdrop_path}/>
+         <Image preview={false} height={480} src={"https://image.tmdb.org/t/p/w500/"+details.backdrop_path}/>
           <Layout className={styles.movie__sub}>
             <Title className={styles.movie__title}>{details.title}</Title>
           </Layout>
        </Layout>
        <Row className={styles.movie__wrapper}>
-         <Col className={styles.movie__poster} span={10}><Image width={480} height={720} src={"https://image.tmdb.org/t/p/w500/"+details.poster_path}/></Col>
+         <Col className={styles.movie__poster} span={10}><Image preview={false}  width={480} height={720} src={"https://image.tmdb.org/t/p/w500/"+details.poster_path}/></Col>
          <Col offset={3} span={10}>
             <Title className={styles.movie__poster__title}>{details.title}</Title>
             <Content className={styles.movie__poster__desc}>{details.overview}</Content>
-            <Content className={styles.movie__raiting}><Image width={14} height={14} src={star}/><Content style={{"marginLeft": "10px"}}>{+details.vote_average?.toFixed(1)}</Content></Content>
+            <Content className={styles.movie__raiting}><Image preview={false}  width={14} height={14} src={star}/><Content style={{"marginLeft": "10px"}}>{+details.vote_average?.toFixed(1)}</Content></Content>
             <Title level={4} className={styles.movie__poster__subtitle}>type</Title>
             <Content className={styles.movie__poster__content}>{details.budget?"movie":"tw-show"}</Content>
             <Title className={styles.movie__poster__subtitle}>Release Date:</Title>
