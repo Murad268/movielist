17dc117eeb2f85364adcc9db95a4872c2c705fd8
Murad Agamedov <82538242+Murad268@users.noreply.github.com>
2022-08-29 11:26:56 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import App from './App';
 const store = createStore(rootReducer, compose(
   applyMiddleware(
     thunk,
-  ),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 ))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
