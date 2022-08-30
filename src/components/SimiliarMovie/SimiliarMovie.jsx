d24a6826e29cd89f37019fade39e690d65810c11
Murad268 <82538242+Murad268@users.noreply.github.com>
2022-08-30 22:25:42 +0400

    s

---
+++ src/components/SimiliarMovie/SimiliarMovie.jsx
@@ -0,0 +1,42 @@
+import React, {useEffect} from 'react';
+import styles from './similiarMovie.module.scss'
+import { actions } from '../../store/actions/actionIndex';
+import { useDispatch, useSelector } from 'react-redux';
+import { Card, Col, Layout } from 'antd';
+import { icons } from 'antd/lib/image/PreviewGroup';
+import { Link, useLocation } from 'react-router-dom';
+const SimiliarMovie = ({movie}) => {
+   const dispatch = useDispatch()
+   const {pathname} = useLocation()
+
+   const data = useSelector(state => {
+      return {
+         favorites: state.favoritesReducer?.favorites,
+         watchers: state.watchReducer?.watchList
+      }
+      })
+ 
+   return (
+      <Col span={6}>
+         <Link to={`/movie/${pathname.split("/")[1]}${movie.id}`}>
+         <Card className={styles.card} cover={<img src={movie.poster_path?`https://image.tmdb.org/t/p/w500/${movie.poster_path}`:icons.notFound} alt="" />}>  
+            <Layout.Content className={styles.card__raiting}><i className="fa-regular fa-star"></i>{String(+movie.vote_average?.toFixed(1)).length < 2 ? String(+movie.vote_average?.toFixed(1)) + ".0" : String(+movie.vote_average?.toFixed(1))}</Layout.Content>
+            <Layout.Content className={styles.card__addFavorites}>
+               {data.favorites?.some(film=>film.id===movie.id)?
+               <div onClick={() => dispatch(actions.removeFavorites(movie.id))}><i className="fa fa-heart" style={{"color": "orange"}} ></i></div>:
+               <div onClick={() => dispatch(actions.addFavorites(movie))}><i className="fa-regular fa-heart"  style={{"color": "orange"}}></i></div>}
+            </Layout.Content>
+            <Layout.Content className={styles.card__addWatchList}>
+               {data.watchers?.some(film=>film.id===movie.id)?
+               <div onClick={() => dispatch(actions.addWatchList(movie))}><i style={{"color": "orange"}} className="fas fa-bookmark"></i></div>:
+               <div onClick={() => dispatch(actions.removeWatchList(movie.id))}><i style={{"color": "orange"}} className="fa-regular fa-bookmark"></i></div>}
+            </Layout.Content>
+            <Layout.Content className={styles.addWatchList}></Layout.Content>
+            <Layout.Content className={styles.card__title}>{movie.title.length<16?movie.title:movie.title.slice(0, 16)+"..."}</Layout.Content>
+         </Card>
+      </Link>
+   </Col>
+   );
+};
+
+export default SimiliarMovie;
--- src/components/SimiliarMovies/SimiliarMovies.jsx
+++ src/components/SimiliarMovies/SimiliarMovies.jsx
@@ -3,10 +3,9 @@ import styles from './similiar.module.scss'
 import { actions } from '../../store/actions/actionIndex';
 import { useDispatch, useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Typography, Card, Col, Layout } from 'antd';
-import { icons } from 'antd/lib/image/PreviewGroup';
-import { Link, useLocation } from 'react-router-dom';
-import MainMovie from '../MainMovie/MainMovie';
+import { Typography } from 'antd';
+import {  useLocation } from 'react-router-dom';
+import SimiliarMovie from '../SimiliarMovie/SimiliarMovie';
 import 'swiper/css';
 const SimiliarMovies = ({id, name}) => {
    const dispatch = useDispatch()
@@ -14,44 +13,19 @@ const SimiliarMovies = ({id, name}) => {
    useEffect(() => {
       dispatch(actions.loadSimiliars(id))
    }, [id]) // eslint-disable-line react-hooks/exhaustive-deps
-   const data = useSelector(state => {
-      return {
-         movies: state.appReducer.similiar.results,
-         favorites: state.favoritesReducer?.favorites,
-         watchers: state.watchReducer?.watchList
-      }
-      })
+   const movies = useSelector(state => state.appReducer.similiar.results)
  
 
-
-      console.log(data?.movies)
+    
    return (
       <>
          <div style={{"paddingTop": "150px"}} >
             <Typography.Title style={{"textAlign": "center"}} className={styles.inner__name}>Recommended Movies</Typography.Title>
             <Swiper spaceBetween={5} slidesPerView={4}>
                   {
-                     data.movies?.map(sim => {
-                        return <SwiperSlide key={sim.id}>
-                                 <Col span={6}>
-                                    <Link to={`/movie/${pathname.split("/")[1]}${sim.id}`}>
-                                    <Card className={styles.card} cover={<img src={sim.poster_path?`https://image.tmdb.org/t/p/w500/${sim.poster_path}`:icons.notFound} alt="" />}>  
-                                       <Layout.Content className={styles.card__raiting}><i className="fa-regular fa-star"></i>{String(+sim.vote_average?.toFixed(1)).length < 2 ? String(+sim.vote_average?.toFixed(1)) + ".0" : String(+sim.vote_average?.toFixed(1))}</Layout.Content>
-                                       <Layout.Content className={styles.card__addFavorites}>
-                                          {data.favorites?.some(film=>film.id===sim.id)?
-                                          <div onClick={() => dispatch(actions.removeFavorites(sim.id))}><i className="fa fa-heart" style={{"color": "orange"}} ></i></div>:
-                                          <div onClick={() => dispatch(actions.addFavorites(sim))}><i className="fa-regular fa-heart"  style={{"color": "orange"}}></i></div>}
-                                       </Layout.Content>
-                                       <Layout.Content className={styles.card__addWatchList}>
-                                          {data.watchers?.some(film=>film.id===sim.id)?
-                                          <div onClick={() => dispatch(actions.addWatchList(sim))}><i style={{"color": "orange"}} className="fas fa-bookmark"></i></div>:
-                                          <div onClick={() => dispatch(actions.removeWatchList(sim.id))}><i style={{"color": "orange"}} className="fa-regular fa-bookmark"></i></div>}
-                                       </Layout.Content>
-                                       <Layout.Content className={styles.addWatchList}></Layout.Content>
-                                       <Layout.Content className={styles.card__title}>{sim.title.length<16?sim.title:sim.title.slice(0, 16)+"..."}</Layout.Content>
-                                    </Card>
-                                    </Link>
-                                 </Col>
+                     movies?.map(movie => {
+                        return <SwiperSlide key={movie.id}>
+                                 <SimiliarMovie movie={movie}/>
                            </SwiperSlide>
                      })
                   }
